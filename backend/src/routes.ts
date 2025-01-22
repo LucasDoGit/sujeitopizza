e80774492bfff5655ef86cc66953caa2e4bef1b4
lucastimoteo <95625902+LucasDoGit@users.noreply.github.com>
2025-01-22 17:51:32 -0300

    feat: criação e remoção de pedidos

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,34 +1,45 @@
 import { Router, Response, Request } from "express";
 import multer from "multer";
 
+import { isAuthenticated } from "./middlewares/isAuthenticated";
+
 import { CreateUserController } from './controllers/User/CreateUserController'
 import { AuthUserController } from './controllers/User/AuthUserController'
 import { DetailUserController } from "./controllers/User/DetailUserController";
 
-import { isAuthenticated } from "./middlewares/isAuthenticated";
 import { CreateCategoryController } from "./controllers/categorie/CreateCategoryController";
 import { ListCategoryController } from "./controllers/categorie/ListCategoryController";
+
 import { CreateProductController } from "./controllers/product/CreateProductController";
 
-import uploadConfig from './config/multer'
 import { ListByCategoryController } from "./controllers/product/ListByCategoryController";
+
+import { CreateOrderController } from "./controllers/order/CreateOrderController";
+import { RemoveOrderController } from "./controllers/order/RemoveOrderController";
+
+import uploadConfig from './config/multer'
 
 const router = Router();
 
 const upload = multer(uploadConfig.upload("./tmp"))
 
-// -- ROTAS USER --
+// -- ROUTES USER --
 router.post('/users', new CreateUserController().handle)
 router.post('/session', new AuthUserController().handle)
 router.get('/me', isAuthenticated, new DetailUserController().handle)
 
-// -- ROTAS CATEGORIAS --
+// -- ROUTES CATEGORY --
 router.post('/category', isAuthenticated, new CreateCategoryController().handle)
 router.get('/category', isAuthenticated, new ListCategoryController().handle)
 
-// -- ROTAS PRODUCT --
+// -- ROUTES PRODUCT --
 router.post('/product', isAuthenticated, upload.single('file'), new CreateProductController().handle)
 
+// -- ROUTES FILTER PRODUCTS --
 router.get('/category/product', isAuthenticated, new ListByCategoryController().handle)
+
+// -- ROUTES ORDER --
+router.post('/order', isAuthenticated, new CreateOrderController().handle)
+router.delete('/order', isAuthenticated, new RemoveOrderController().handle)
 
 export { router }
